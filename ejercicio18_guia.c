66cfb8d43bca4efcf071fca5e19d62c5c0fe8e83
matipalmieri <matiaspalmieri20@gmail.com>
2024-05-17 15:40:26 -0300

    ejercicio 18 de la guia de lenguajes electronicos

---
+++ ejercicio18_guia.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+float const pi=3.14
+
+int main(){
+
+    float area,radio;
+    printf("Ingrese el radio tu circulo: ");
+    scanf("%f",&radio);
+    area=pi*radio*radio;
+    printf("El area de tu circulo es igual a %f metros cuadrados",area);
+    return 0;
+}
