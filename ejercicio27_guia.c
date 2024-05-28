ae3395431c6e7990591b66d2f2f17f7d3558a4e0
matipalmieri <matiaspalmieri20@gmail.com>
2024-05-28 11:48:07 -0300

    ejercicios

--- ejercicio27_guia.c
+++ ejercicio27_guia.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
-    
+    int cifra1,cifra2,cifra3,numero;
+    printf("Ingrese un número entero de 3 cifras: ");
+    scanf("%d",&numero);
+    cifra1=numero/100;
+    cifra2=(numero%100)/10;
+    cifra3=(numero%100)%10;
+    printf("La cifra con menor valor del número que ingresaste es %d, la cifra del medio es vale %d y la que tiene más valor vale %d",cifra3,cifra2,cifra1);
     return 0;
 }
---
+++ ejercicio32_guia.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+    int numero,checkeo;
+    printf("Ingrese un número entero: ");
+    scanf("%d",&numero);
+    checkeo=numero%2;
+    if(checkeo==0){
+        printf("El número que ingresaste es par.");
+    }
+    else if(checkeo==1){
+        printf("El número que ingresaste es impar.");
+    }
+    return 0;
+}
