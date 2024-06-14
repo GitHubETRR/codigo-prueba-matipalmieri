559ba3836ee10ea9c772c11e5960aed3ee5b57ee
matipalmieri <matiaspalmieri20@gmail.com>
2024-06-14 12:20:29 -0300

    ejercicios

--- ejercicio55_guia.c
+++ ejercicio55_guia.c
@@ -4,6 +4,7 @@ comprendidos entre el primer número y el segundo. Tener en cuenta que el primer
 al segundo y validar que ingrese las letras correspondientes.*/
 #include <stdio.h>
 #include <math.h>
+
 int main() {
     int num1, num2;
     char letra;
@@ -13,27 +14,27 @@ int main() {
     scanf("%d", &num2);
     if (num1 >= num2) {
         printf("El primer número debe ser menor que el segundo.\n");
-        return 1;
+        return 0;
     }
     printf("Ingrese 'p' para pares o 'i' para impares: ");
     scanf(" %c", &letra);
     if (letra == 'p' || letra == 'P') {
         printf("Números pares entre %d y %d:\n", num1, num2);
-        for (int i = num1; i <= num2; i++) {
+        for (int i = num1+1; i < num2; i++) {
             if (i % 2 == 0) {
                 printf("%d ", i);
             }
         }
     } else if (letra == 'i' || letra == 'I') {
         printf("Números impares entre %d y %d:\n", num1, num2);
-        for (int i = num1; i <= num2; i++) {
+        for (int i = num1+1; i < num2; i++) {
             if (i % 2 != 0) {
                 printf("%d ", i);
             }
         }
     } else {
         printf("Letra inválida. Por favor, ingrese 'p' o 'i'.\n");
-        return 1;
+        return 0;
     }
     printf("\n");
     return 0;
---
+++ ejercicio56_guia.c
@@ -0,0 +1,3 @@
+/*56.	Se realizó un concurso de tiro al blanco. Existen 5 participantes y cada uno de ellos efectúa 10
+disparos, registrando las coordenadas (x, y) de cada disparo. Indicar cuántos disparos se produjeron en 
+cada cuadrante y cuantos dieron en el blanco.*/
