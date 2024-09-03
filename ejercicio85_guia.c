5eeea44f4d80c8be0e99d3d55361f3b873feed10
matipalmieri <matiaspalmieri20@gmail.com>
2024-09-03 11:42:31 -0300

    cambios

--- ejercicio85_guia.c
+++ ejercicio85_guia.c
@@ -3,12 +3,13 @@ de la Quiniela. El ingreso de los datos será indicando posición-numero, de mod
 posición 5 antes que la posición 2.*/
 
 #include <stdio.h>
+#define CASILLAS 10
 
 int main(){
-    int vect[10]={0};
+    int vect[CASILLAS]={0};
     int c,val;
     for(int i=0;i<11;i++){
-        printf("¿A qué casilla de las 10 le quiere introducir el valor?(te lo voy a preguntar hasta que hayas llenado todos los números)\n");
+        printf("¿A qué casilla del 1-10 le quiere introducir el valor?(quedan %d casillas)\n",10-i);
         scanf("%d",&c);
         printf("¿Cuál es el valor?\n");
         scanf("%d",&val);
